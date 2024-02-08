1f0ff1157c4ac8b62fe3a3639bf3bdb80678d93d
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-08 21:51:22 +0100

    setup flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
+import { initFlowbite } from "flowbite";
 
 @Component({
   selector: "app-root",
@@ -9,6 +10,10 @@ import { RouterOutlet } from "@angular/router";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "solution_challenge";
+
+  ngOnInit() {
+    initFlowbite();
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-    "./src/**/**/*.{html,ts}",
-    "./src/**/**/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
