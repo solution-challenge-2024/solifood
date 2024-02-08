a7d18f5811c44fe19c9c1b5783655597ed01945b
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-02-08 19:44:03 +0100

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./src/**/**/*.{html,ts}",
+    "./src/**/**/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
