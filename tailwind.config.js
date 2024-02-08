d164254db7c5c0726dbdb1671dcf3d1cebce9e6c
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-08 22:10:26 +0100

    use google sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Product Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
