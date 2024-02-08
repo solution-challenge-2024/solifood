c04623b00d7d5be09d26fde53ff4e402d73b714e
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-08 21:06:52 +0100

    add database entities interfaces

---
+++ src/app/models/basket.ts
@@ -0,0 +1,21 @@
+import { Timestamp } from "@angular/fire/firestore";
+import { User } from "./user";
+
+export interface Basket {
+  id: string;
+  title: string;
+  description: string;
+  images: string[];
+  realPrice: number;
+  price: number;
+  location: { lat: number; lon: number };
+  available: boolean;
+  blocked: boolean;
+  tags: string[];
+  ingredients: string[];
+  createdBy: User;
+  claimedBy?: User;
+  expiredAt: Timestamp;
+  soldAt?: Timestamp;
+  createdAt: Timestamp;
+}
