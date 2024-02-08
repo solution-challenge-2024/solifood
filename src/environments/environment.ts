2f83d6ec91d5c3ca11492651a6a3771fcd0dc354
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-08 20:35:50 +0100

    firebase integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  useEmulators: true,
+  firebaseConfig: {
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    authDomain: 'project-id.firebaseapp.com',
+    projectId: 'project-id',
+    storageBucket: 'project-id.appspot.com',
+    messagingSenderId: 'XXXXXXXXXXXXXX',
+    appId: 'X:XXXXXXXXXXXXX:web:XXXXXXXXXXXXXXXX',
+    measurementId: 'G-XXXXXXXXXX',
+  },
+};
