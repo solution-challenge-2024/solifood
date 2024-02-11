5cae8ca3bd6f0451360cada59c8d7623dff40931
AhmedHoussamBouzine <ahmedhoussam.bouzine.01@gmail.com>
2024-02-11 19:49:18 +0100

    Revert "ignore dev environment file"
    This reverts commit a72587592d6466c9f5cc0ad7df68bea2a5361bd1.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  useEmulators: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAllQLsNCEDguNXjdQ02gfiliXWelBTES8",
+    authDomain: "solifood.firebaseapp.com",
+    projectId: "solifood",
+    storageBucket: "solifood.appspot.com",
+    messagingSenderId: "1084068536570",
+    appId: "1:1084068536570:web:494ba8cd41f7000d88e943",
+    measurementId: "G-M6P7690TWK",
+  },
+};
