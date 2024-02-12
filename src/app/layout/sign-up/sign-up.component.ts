0b5c62349151f62901e66846c4fd3df0b7922e6b
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-12 21:04:46 +0100

    signup page ui implementation

--- src/app/layout/sign-up/sign-up.component.ts
+++ src/app/layout/sign-up/sign-up.component.ts
@@ -1,10 +1,31 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-sign-up",
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: "./sign-up.component.html",
-  styleUrl: "./sign-up.component.scss",
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  showPassword = false;
+  buttonsDisabled = false;
+  signupForm = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl(""),
+  });
+
+  handleSubmit() {
+    this.buttonsDisabled = true;
+
+    console.log(this.signupForm.value);
+  }
+
+  handleGoogleSignIn() {
+    this.buttonsDisabled = true;
+    console.log("Sign in with Google");
+  }
+}
