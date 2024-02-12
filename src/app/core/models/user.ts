eaabee56b5269dcd0004b214428e07cdc746ab2a
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2024-02-12 12:15:22 +0100

    `fix` : models

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -1,1 +1,14 @@
-export interface User {}
+import { Timestamp } from "@angular/fire/firestore";
+
+export interface User {
+  id: string;
+  first_name: string;
+  last_name: string;
+  picture: string;
+  email: string;
+  location: { lat: number; lon: number };
+  ratings: { rating: number; by: string }[];
+  blocked: string[];
+  lastLogin: Timestamp;
+  joinedAt: Timestamp;
+}
