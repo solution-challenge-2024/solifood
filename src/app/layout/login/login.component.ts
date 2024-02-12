57ad6e6b3038fd89b9961e0ddc464b1ba43dbf1e
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-12 20:57:23 +0100

    login page ui implementation

--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-login",
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: "./login.component.html",
-  styleUrl: "./login.component.scss",
 })
-export class LoginComponent {}
+export class LoginComponent {
+  showPassword = false;
+  buttonsDisabled = false;
+  loginForm = new FormGroup({
+    email: new FormControl(""),
+    password: new FormControl(""),
+  });
+
+  handleSubmit() {
+    this.buttonsDisabled = true;
+
+    console.log("email: ", this.loginForm.value.email);
+    console.log("password: ", this.loginForm.value.password);
+  }
+
+  handleGoogleSignIn() {
+    this.buttonsDisabled = true;
+    console.log("Sign in with Google");
+  }
+}
