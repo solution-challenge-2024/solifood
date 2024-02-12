5fa4569a955d5efa2cf6ed1a2a5d5e989b30328c
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-12 21:14:00 +0100

    footer component ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { initFlowbite } from "flowbite";
 import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: "./app.component.html",
 })
 export class AppComponent implements OnInit {
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-footer",
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: "./footer.component.html",
-  styleUrl: "./footer.component.scss",
 })
-export class FooterComponent {}
+export class FooterComponent {
+  links = [
+    { label: "About", path: "/about" },
+    { label: "Contact", path: "/contact" },
+    { label: "Terms of Service", path: "/terms-of-service" },
+    { label: "Privacy Policy", path: "/privacy-policy" },
+  ];
+  currYear = new Date().getFullYear();
+}
