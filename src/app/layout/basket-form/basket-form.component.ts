1fda0d68d789f36584b4a5a7479a2ebe48d3afbe
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-16 22:20:33 +0100

    show map in the basket form

--- src/app/layout/basket-form/basket-form.component.ts
+++ src/app/layout/basket-form/basket-form.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { ButtonComponent } from "../../components/button/button.component";
 import { InputComponent } from "../../components/input/input.component";
 import { FileInputComponent } from "../../components/file-input/file-input.component";
 import { TagsInputComponent } from "../../components/tags-input/tags-input.component";
 import { ChoiceComponent } from "../../components/choice/choice.component";
+import { StorageService } from "../../core/data/storage.service";
+import { MapComponent } from "../../components/map/map.component";
 
 @Component({
   selector: "app-basket-form",
@@ -16,10 +18,13 @@ import { ChoiceComponent } from "../../components/choice/choice.component";
     FileInputComponent,
     TagsInputComponent,
     ChoiceComponent,
+    MapComponent,
   ],
   templateUrl: "./basket-form.component.html",
 })
 export class BasketFormComponent {
+  public storage = inject(StorageService);
+
   basket = {
     title: "",
     description: "",
@@ -29,9 +34,11 @@ export class BasketFormComponent {
     price: 0,
     expiredAt: new Date(),
     isAvailable: true,
+    location: { latitude: 33.589886, longitude: -7.603869 },
   };
 
   loading = false;
+  mapCenter = { latitude: 33.589886, longitude: -7.603869 };
 
   async saveBasket() {
     if (this.loading) return;
@@ -40,5 +47,9 @@ export class BasketFormComponent {
     console.log(this.basket);
 
     // TODO: Implement API call to save the basket
+  }
+
+  locationChange(location: { latitude: number; longitude: number }) {
+    this.basket.location = location;
   }
 }
