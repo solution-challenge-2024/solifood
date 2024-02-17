564f3b9c501fca34fb937d2ea81e1d337a998a2d
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-17 12:14:02 +0100

    image slider in basket details page implementation

--- src/app/layout/basket/basket.component.ts
+++ src/app/layout/basket/basket.component.ts
@@ -18,7 +18,9 @@ export class BasketComponent {
       "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker",
     images: [
       "https://images.radio-canada.ca/v1/alimentation/recette/16x9/ogleman-spaghetti-boulettes.jpg",
-      "https://images.radio-canada.ca/v1/alimentation/recette/16x9/ogleman-spaghetti-boulettes.jpg",
+      "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg",
+      "https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg",
+      "https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg",
     ],
     realPrice: 50,
     price: 40,
@@ -43,6 +45,17 @@ export class BasketComponent {
     expiredAt: Timestamp.now(),
     createdAt: Timestamp.now(),
   };
+
+  activeImage = 0;
+
+  willExpireSoon(): boolean {
+    if (!this.basket) return false;
+
+    const now = dayjs();
+    const willExpireAt = dayjs(this.basket.expiredAt.toDate());
+
+    return willExpireAt.diff(now, "day") < 1; // less than 1 day
+  }
 
   timeAgo(date: Date): string {
     return dayjs(date).fromNow();
